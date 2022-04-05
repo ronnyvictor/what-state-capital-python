21809f9324bf632b425be10ac27057c241f136e4
Roneil Victor <roneilvictor@ronny.local>
2022-04-05 09:37:45 -0400

    typo

--- capital.py
+++ capital.py
@@ -11,7 +11,7 @@ nstates = int(input("How many state capitals would you like to guess?"))
 states = states[0:nstates]
 
 for state in states:
-    answer = input(f"What state is the capital of {state['name']}?")
+    answer = input(f"What is the capital of {state['name']}?")
     if answer.lower() == state["capitol"].lower():
         print(f"Correct! The capital of {state['name']} is {state['capitol']}.")
     else:
